388209508f183fc46f047d586340b737703b55f5
LeeSeungHyun <iop2480@naver.com>
2020-09-30 22:13:32 +0900

    app.js 수정

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  // const [ name, setName ] = useState('shaun');
+  // const [ age, setAge ] = useState('31');
+  const [ people, setPeople ] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+    { name: 'bowser2', key: '8' },
+    { name: 'bowser3', key: '9' }
+  ]);
+
+
+  // const clickHandler = () => {
+  //   setName('chun-li');
+  //   setPerson({ name: 'lee', age: 31 });
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Enter name: </Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)}
+      />
+    
+      <Text>Enter age: </Text>
+      <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 99'
+        onChangeText={(val) => setAge(val)}
+      />
+      <Text>name: {name}, age: {age}</Text> */}
+      <ScrollView>
+        { people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -15,7 +58,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center'
   },
+  item: {
+    marginTop: 24,
+    padding: 20,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
 });
