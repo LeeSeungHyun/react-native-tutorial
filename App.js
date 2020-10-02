bbcdb233ad47fff6971eab3a07872e42ad4ecbb8
LeeSeungHyun <iop2480@naver.com>
2020-10-02 17:05:29 +0900

    finish project

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
   // const [ name, setName ] = useState('shaun');
   // const [ age, setAge ] = useState('31');
-  const [ people, setPeople ] = useState([
-    { name: 'shaun', key: '1' },
-    { name: 'yoshi', key: '2' },
-    { name: 'mario', key: '3' },
-    { name: 'luigi', key: '4' },
-    { name: 'peach', key: '5' },
-    { name: 'toad', key: '6' },
-    { name: 'bowser', key: '7' },
-    { name: 'bowser2', key: '8' },
-    { name: 'bowser3', key: '9' }
-  ]);
+  // const [ people, setPeople ] = useState([
+  //   { name: 'shaun', id: '1' },
+  //   { name: 'yoshi', id: '2' },
+  //   { name: 'mario', id: '3' },
+  //   { name: 'luigi', id: '4' },
+  //   { name: 'peach', id: '5' },
+  //   { name: 'toad', id: '6' },
+  //   { name: 'bowser', id: '7' },
+  //   { name: 'bowser2', id: '8' },
+  //   { name: 'bowser3', id: '9' }
+  // ]);
+
+  const [ todos, setTodos ] = useState([
+    { text: 'buy beverage', key: '1' },
+    { text: 'practice singing 1 hour', key: '2' },
+    { text: 'react native lecture by 15', key: '3' }
+  ])
 
 
   // const clickHandler = () => {
@@ -23,50 +33,77 @@ export default function App() {
   //   setPerson({ name: 'lee', age: 31 });
   // }
 
+  // const pressHandler = (id) => {
+  //   console.log(id)
+  //   setPeople((prevPeople) => {
+  //     return prevPeople.filter(person => person.id != id);
+  //   })
+  // }
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('Oops!', 'toDos Must be over 3 chars long', [
+        {text: 'understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+  } 
+
   return (
-    <View style={styles.container}>
-      {/* <Text>Enter name: </Text>
-      <TextInput 
-        multiline
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)}
-      />
-    
-      <Text>Enter age: </Text>
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='e.g. 99'
-        onChangeText={(val) => setAge(val)}
-      />
-      <Text>name: {name}, age: {age}</Text> */}
-      <ScrollView>
-        { people.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          )
-        })}
-      </ScrollView>
-    </View>
+    // <Sandbox />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
+      <View style={styles.container}>
+        {/* header */}
+        <Header />
+        <View style={styles.content}>
+          {/* to form */}
+          <AddTodo 
+            submitHandler={submitHandler}
+          />
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem 
+                  item={item}
+                  pressHandler={pressHandler}
+                />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: 'center',
-    // justifyContent: 'center'
+    backgroundColor: '#fff'
   },
-  item: {
-    marginTop: 24,
-    padding: 20,
-    backgroundColor: 'pink',
-    fontSize: 24
+  content: {
+    padding: 40,
+    flex: 1
+  },
+  list: {
+    flex: 1,
+    marginTop: 20
   }
 });
